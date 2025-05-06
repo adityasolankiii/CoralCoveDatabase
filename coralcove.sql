0a9e749b6beb6431770b564db99be36771380708
Aditya Solanki <144944613+adityasolankiii@users.noreply.github.com>
2025-05-07 01:15:11 +0530

    Add files via upload

---
+++ coralcove.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3242
+-- Generation Time: May 06, 2025 at 09:40 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `coralcove`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rooms`
+--
+-- Error reading structure for table coralcove.rooms: #1932 - Table &#039;coralcove.rooms&#039; doesn&#039;t exist in engine
+-- Error reading data for table coralcove.rooms: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `coralcove`.`rooms`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room_book`
+--
+-- Error reading structure for table coralcove.room_book: #1932 - Table &#039;coralcove.room_book&#039; doesn&#039;t exist in engine
+-- Error reading data for table coralcove.room_book: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `coralcove`.`room_book`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table coralcove.users: #1932 - Table &#039;coralcove.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table coralcove.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `coralcove`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
